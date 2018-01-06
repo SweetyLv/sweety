2392816cdb8731caffee4924c83187e03ab57ee2
lovely_sweety <445294960@qq.com>
2018-01-06 23:31:24 +0800

    Update reqResponse.js

--- nodeJs/reqResponse.js
+++ nodeJs/reqResponse.js
@@ -8,7 +8,8 @@ server.on('listening',function(){
 });
 server.on('request',function(req,res){
     //console.log('req');
-    res.write('hello');
+    res.write('<h1>hello</h1>');
+    //服务器请求后，显示的内容
     res.end();
 });
 server.listen(8080,'localhost');
